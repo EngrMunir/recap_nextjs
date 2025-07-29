03d3dcf605bc028cb631ec75f979ae276a9dd9ac
munir <smsirajulmonir@gmail.com>
2025-07-29 12:24:55 +0600

    google font, metadata optimization explord

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export const metadata ={
+    title:"About Us"
+}
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+        </div>
+    );
+};
+
+export default AboutPage;
